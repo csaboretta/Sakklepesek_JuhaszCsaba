c0d5e541c6cc65aa43deaced5e16b3f005e13760
csaboretta <92793579+csaboretta@users.noreply.github.com>
2022-05-29 12:32:55 +0200

    Add files via upload
    Létrehoztam a sakktábla osztályt, és a brushokat

---
+++ Sakklepesek_JuhaszCsaba2/Sakklepesek_JuhaszCsaba2/SakkTabla.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Sakklepesek_JuhaszCsaba2
+{
+    internal class SakkTabla
+    {
+        private readonly Brush DARK = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+        private readonly Brush LIGHT = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+        private readonly Brush MOVE_DARK = new SolidColorBrush(Color.FromRgb(50, 50, 50));
+        private readonly Brush MOVE_LIGHT = new SolidColorBrush(Color.FromRgb(205, 205, 205));
+    }
+}
---
+++ Sakklepesek_JuhaszCsaba2/Sakklepesek_JuhaszCsaba2/pieces/Sakktábla.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sakklepesek_JuhaszCsaba2.pieces
+{
+    internal class Sakktábla
+    {
+    }
+}
