36b17abf707fc533402a567f970c98b62b96ac61
csaboretta <92793579+csaboretta@users.noreply.github.com>
2022-05-29 12:49:24 +0200

    Add files via upload
    Kész a sorszámozás, valamiért a gridet nem érem el.

--- Sakklepesek_JuhaszCsaba2/Sakklepesek_JuhaszCsaba2/SakkTabla.cs
+++ Sakklepesek_JuhaszCsaba2/Sakklepesek_JuhaszCsaba2/SakkTabla.cs
@@ -1,21 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
-namespace Sakklepesek_JuhaszCsaba2
+namespace Chess;
+
+public class SakkTabla
 {
-    internal class SakkTabla
+    private readonly Brush DARK = new SolidColorBrush(Color.FromRgb(209, 139, 71));
+    private readonly Brush LIGHT = new SolidColorBrush(Color.FromRgb(255, 206, 158));
+    private readonly Brush MOVE_DARK = new SolidColorBrush(Color.FromRgb(209, 190, 171));
+    private readonly Brush MOVE_LIGHT = new SolidColorBrush(Color.FromRgb(255, 233, 209));
+    private Dictionary<int, Dictionary<int, Rectangle>> tiles = new();
+    private int baseX;
+    private int baseY;
+
+    private Image? CurrentImage;
+
+    public Tabla(int bottomLeftX, int bottomLeftY)
     {
-        private readonly Brush DARK = new SolidColorBrush(Color.FromRgb(0, 0, 0));
-        private readonly Brush LIGHT = new SolidColorBrush(Color.FromRgb(255, 255, 255));
-        private readonly Brush MOVE_DARK = new SolidColorBrush(Color.FromRgb(50, 50, 50));
-        private readonly Brush MOVE_LIGHT = new SolidColorBrush(Color.FromRgb(205, 205, 205));
+        
+        baseX = bottomLeftX;
+        baseY = bottomLeftY;
+        for (int row = 1; row <= 8; row++)
+        {
+            int y = bottomLeftY - (row - 1) * 60;
+            var number = new Label();
+            number.Content = "" + row;
+            number.HorizontalAlignment = HorizontalAlignment.Left;
+            number.VerticalAlignment = VerticalAlignment.Top;
+            number.Width = 60;
+            number.Height = 27;
+            number.Margin = new Thickness(bottomLeftX - 20, y + 20, 0, 0);
+            grid.Children.Add(number);
+          
+
+        }
+       
     }
+
+
+
+   
 }
